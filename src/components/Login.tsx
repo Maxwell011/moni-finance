f03e8442f9258ce0e651a37e73e87dc4db09b142
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-05 21:31:43 +0100

    Add login form to Login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,19 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h2>Log in</h2>
+
+      <label>Email</label>
+      <input type='text' placeholder='Enter Your Email' />
+      <label>Password</label>
+      <input type='password' placeholder='Enter Your Password' />
+
+      <h3>Forget Password?</h3>
+
+      <button className='flex w-[278px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px]'>
+        Login in
+      </button>
+    </div>
+  );
 };
 export default Login;
