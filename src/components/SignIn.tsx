f7c11766a5a10dec9b7863a427ad148dd9099c98
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-05 21:32:05 +0100

    Simplify SignIn component by removing unnecessary elements.

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,19 +1,6 @@
 const SignIn = () => {
   return (
-    <div>
-      <h2>SignIn</h2>
-
-      <label>Email</label>
-      <input type='text' placeholder='Enter Your Email' />
-      <label>Password</label>
-      <input type='password' placeholder='Enter Your Password' />
-
-      <h3>Forget Password?</h3>
-
-      <button className='flex w-[278px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px]'>
-        Sign in
-      </button>
-    </div>
-  );
-};
-export default SignIn;
+    <div>SignIn</div>
+  )
+}
+export default SignIn
