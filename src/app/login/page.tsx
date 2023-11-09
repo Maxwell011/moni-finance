8bbfd8a0c735f7a13a6899589c5d6525eb5d1a40
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-09 21:44:27 +0100

    Refactor login page input fields

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,23 +5,26 @@ const Login = () => {
         <h2 className='text-[#28374B] text-[32px] not-italic font-semibold leading-[normal] mb-10'>
           Log in
         </h2>
-
-        <label className='text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
-          Email
-        </label>
-        <input
-          type='text'
-          placeholder='Enter Your Email'
-          className='w-full sm:w-36 h-10 sm:h-[41px] input-style mb-4'
-        />
-        <label className='sm:text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
-          Password
-        </label>
-        <input
-          type='password'
-          placeholder='Enter Your Password'
-          className='w-full sm:w-36 h-10 sm:h-[41px] input-style'
-        />
+        <div className='flex flex-col gap-1'>
+          <label className='text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
+            Email
+          </label>
+          <input
+            type='text'
+            placeholder='Enter Your Email'
+            className='w-full sm:w-36 h-10 sm:h-[41px] input-style mb-4'
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label className='sm:text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
+            Password
+          </label>
+          <input
+            type='password'
+            placeholder='Enter Your Password'
+            className='w-full sm:w-36 h-10 sm:h-[41px] input-style'
+          />
+        </div>
 
         <h3 className='text-xl text-[#0476D9] font-normal leading-[normal] mt-4 mb-8'>
           Forget Password?
