71081641772b8a718b23dfdc40e9d5ee38452244
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-12 22:28:54 +0100

    Refactor LaunchScreen component

--- src/app/components/LaunchScreen.tsx
+++ src/app/components/LaunchScreen.tsx
@@ -1,32 +1,39 @@
-"use client";
-import Link from "next/link";
-import MoneyIcon from "../svg/MoneyIcon";
+// "use client";
+// import Link from "next/link";
+// import MoneyIcon from "../svg/MoneyIcon";
+
+// const LaunchScreen = () => {
+//   return (
+//     <section>
+//       {/* Top content */}
+//       <div className=' first'>
+//         <div className='second'>
+//           {/* <MoneyIcon /> */}
+//         </div>
+//       </div>
+//       {/* w-[470.001px] h-[492.959px] rotate-[-30.108deg] absolute top-[-220px] rounded-[492.959px] -left-11 bg-[#0476d9] */}
+
+//       {/* Buttons */}
+//       <div className='flex gap-4 flex-col mr-[-21px] ml-[100px] mt-[350px] mb-0'>
+//         <button className='text-[#0476D9] text-base not-italic font-normal leading-[normal] border border-[color:var(--primary-100,#0476D9)] flex w-[278px] justify-center items-center gap-4 p-3 rounded-[10px] border-solid '>
+//           <Link href='/login' legacyBehavior>
+//             Login
+//           </Link>{" "}
+//         </button>
+//         <button className='flex w-[278px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] bg-[#0476D9]'>
+//           <Link href='/signup' legacyBehavior>
+//             Sign up
+//           </Link>
+//         </button>
+//       </div>
+//     </section>
+//   );
+// };
+// export default LaunchScreen;
 
 const LaunchScreen = () => {
   return (
-    <section>
-      {/* Top content */}
-      <div className=' first'>
-        <div className='second'>
-          {/* <MoneyIcon /> */}
-        </div>
-      </div>
-      {/* w-[470.001px] h-[492.959px] rotate-[-30.108deg] absolute top-[-220px] rounded-[492.959px] -left-11 bg-[#0476d9] */}
-
-      {/* Buttons */}
-      <div className='flex gap-4 flex-col mr-[-21px] ml-[100px] mt-[350px] mb-0'>
-        <button className='text-[#0476D9] text-base not-italic font-normal leading-[normal] border border-[color:var(--primary-100,#0476D9)] flex w-[278px] justify-center items-center gap-4 p-3 rounded-[10px] border-solid '>
-          <Link href='/login' legacyBehavior>
-            Login
-          </Link>{" "}
-        </button>
-        <button className='flex w-[278px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] bg-[#0476D9]'>
-          <Link href='/signup' legacyBehavior>
-            Sign up
-          </Link>
-        </button>
-      </div>
-    </section>
-  );
-};
-export default LaunchScreen;
+    <div>LaunchScreen</div>
+  )
+}
+export default LaunchScreen
