8081be0cf0ffecc5cd63b1caa403da9cddd1171e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-12 22:29:03 +0100

    Commented out Formik code and added new page component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,85 +1,85 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from "yup";
 
-const validationSchema = Yup.object({
-  email: Yup.string().email().required("Email is required"),
-  password: Yup.string()
-    .required("Password is required")
-    .min(4, "Password is too short - should be 4 chars min"),
-});
+// const validationSchema = Yup.object({
+//   email: Yup.string().email().required("Email is required"),
+//   password: Yup.string()
+//     .required("Password is required")
+//     .min(4, "Password is too short - should be 4 chars min"),
+// });
 
-const initialValues = {
-  email: "",
-  password: "",
-};
+// const initialValues = {
+//   email: "",
+//   password: "",
+// };
 
-const Login = () => {
-  return (
-    <>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(values: any) => {
-          console.log(values);
-        }}
-      >
-        {(formik) => {
-          const { errors, touched, isValid, dirty } = formik;
-          return (
-            <div className='container'>
-              <h1>Sign in to continue</h1>
-              <Form>
-                <div className='form-row'>
-                  <label htmlFor='email'>Email</label>
-                  <Field
-                    type='email'
-                    name='email'
-                    id='email'
-                    className={
-                      errors.email && touched.email ? "input-error" : null
-                    }
-                  />
-                  <ErrorMessage
-                    name='email'
-                    component='span'
-                    className='error'
-                  />
-                </div>
+// const Login = () => {
+//   return (
+//     <>
+//       <Formik
+//         initialValues={initialValues}
+//         validationSchema={validationSchema}
+//         onSubmit={(values: any) => {
+//           console.log(values);
+//         }}
+//       >
+//         {(formik) => {
+//           const { errors, touched, isValid, dirty } = formik;
+//           return (
+//             <div className='container'>
+//               <h1>Sign in to continue</h1>
+//               <Form>
+//                 <div className='form-row'>
+//                   <label htmlFor='email'>Email</label>
+//                   <Field
+//                     type='email'
+//                     name='email'
+//                     id='email'
+//                     className={
+//                       errors.email && touched.email ? "input-error" : null
+//                     }
+//                   />
+//                   <ErrorMessage
+//                     name='email'
+//                     component='span'
+//                     className='error'
+//                   />
+//                 </div>
 
-                <section className='form-row'>
-                  <label htmlFor='password'>Password</label>
-                  <Field
-                    type='password'
-                    name='password'
-                    id='password'
-                    className={
-                      errors.password && touched.password ? "input-error" : null
-                    }
-                  />
-                  <ErrorMessage
-                    name='password'
-                    component='span'
-                    className='error'
-                  />
-                </section>
+//                 <section className='form-row'>
+//                   <label htmlFor='password'>Password</label>
+//                   <Field
+//                     type='password'
+//                     name='password'
+//                     id='password'
+//                     className={
+//                       errors.password && touched.password ? "input-error" : null
+//                     }
+//                   />
+//                   <ErrorMessage
+//                     name='password'
+//                     component='span'
+//                     className='error'
+//                   />
+//                 </section>
 
-                <button
-                  type='submit'
-                  className={!(dirty && isValid) ? "disabled-btn" : ""}
-                  disabled={!(dirty && isValid)}
-                >
-                  Sign In
-                </button>
-              </Form>
-            </div>
-          );
-        }}
-      </Formik>
-    </>
-  );
-};
+//                 <button
+//                   type='submit'
+//                   className={!(dirty && isValid) ? "disabled-btn" : ""}
+//                   disabled={!(dirty && isValid)}
+//                 >
+//                   Sign In
+//                 </button>
+//               </Form>
+//             </div>
+//           );
+//         }}
+//       </Formik>
+//     </>
+//   );
+// };
 
-export default Login;
+// export default Login;
 // import React from "react";
 
 // const Login = () => {
@@ -122,3 +122,10 @@ export default Login;
 //   );
 // };
 // export default Login;
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+export default page
