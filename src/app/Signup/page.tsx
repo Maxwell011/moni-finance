0e0f32d662b70a0d26f9444cc6884880edd0631f
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-16 14:14:38 +0100

    Refactor Signup page component to implement login functionality

--- src/app/Signup/page.tsx
+++ src/app/Signup/page.tsx
@@ -1,41 +1,34 @@
-// const signUp = () => {
-//   return (
-//     <>
-//       <div className='mx-[41px] my-0'>
-//         <h2 className='text-[#28374B] text-[32px] not-italic font-semibold leading-[normal] mb-10'>
-//           Sign Up
-//         </h2>
-//         <div className='flex flex-col gap-1'>
-//           <label className='text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
-//             Email
-//           </label>
-//           <input
-//             type='text'
-//             placeholder='Enter Your Email'
-//             className='w-full sm:w-36 h-10 sm:h-[41px] input-style mb-4'
-//           />
-//         </div>
-//         <div className='flex flex-col gap-1'>
-//           <label className='sm:text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
-//             Password
-//           </label>
-//           <input
-//             type='password'
-//             placeholder='Enter Your Password'
-//             className='w-full sm:w-36 h-10 sm:h-[41px] input-style'
-//           />
-//         </div>
-
-//         <button className='inactive-btn flex justify-center items-center gap-4 text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] sm:w-36 w-full mt-20'>
-//           Sign Up
-//         </button>
-//       </div>
-//     </>
-//   );
-// };
-// export default signUp;
-
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <>
+      <div className=''>
+        <h2 className='text-[#28374B] text-[32px] not-italic font-semibold leading-[normal] mb-10'>
+          Log in
+        </h2>
+        <label className='text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
+          Email
+        </label>
+        <input
+          type='text'
+          placeholder='Enter Your Email'
+          className='w-full sm:w-36 h-10 sm:h-[41px] input-style mb-4'
+        />
+        <label className='sm:text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
+          Password
+        </label>
+        <input
+          type='password'
+          placeholder='Enter Your Password'
+          className='w-full sm:w-36 h-10 sm:h-[41px] input-style'
+        />
+        <h3 className='text-xl text-[#0476D9] font-normal leading-[normal] mt-4 mb-8'>
+          Forget Password?
+        </h3>
+        <button className='inactive-btn flex justify-center items-center gap-4 text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] sm:w-36 w-full'>
+          Login in
+        </button>
+      </div>
+    </>
+  );
 };
 export default Signup;
