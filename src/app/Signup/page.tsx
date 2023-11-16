4f86403f529e136e318a513407d601dad8f639c3
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-16 14:21:58 +0100

    Update Signup page with Sign Up button

--- src/app/Signup/page.tsx
+++ src/app/Signup/page.tsx
@@ -3,7 +3,7 @@ const Signup = () => {
     <>
       <div className=''>
         <h2 className='text-[#28374B] text-[32px] not-italic font-semibold leading-[normal] mb-10'>
-          Log in
+          Sign Up
         </h2>
         <label className='text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
           Email
@@ -21,11 +21,8 @@ const Signup = () => {
           placeholder='Enter Your Password'
           className='w-full sm:w-36 h-10 sm:h-[41px] input-style'
         />
-        <h3 className='text-xl text-[#0476D9] font-normal leading-[normal] mt-4 mb-8'>
-          Forget Password?
-        </h3>
-        <button className='inactive-btn flex justify-center items-center gap-4 text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] sm:w-36 w-full'>
-          Login in
+        <button className='inactive-btn flex justify-center items-center gap-4 text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] sm:w-36 w-full mt-16'>
+          Sign Up
         </button>
       </div>
     </>
