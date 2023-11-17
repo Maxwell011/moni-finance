97d165f6653e18891e14bead1649eea939f29abd
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-17 13:49:57 +0100

    Updated image div

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
 const Profile = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const handleImageChange = (event: any) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+    setPreviewImage(URL.createObjectURL(file));
+  };
+
+  const handleClick = () => {
+    document.getElementById("profileImageInput").click();
+  };
+
   return (
     <>
       <section className='bg-[#035EAE] w-full h-[246px] lg:w-full'>
         <div className='w-full h-[189px] rounded-[500px_500px_0px_0px] bg-white absolute top-[58px]'>
-          {/* Profile image section*/}
           <div className='flex items-center justify-center mt-12'>
             <div className='first'>
               <svg
@@ -21,26 +35,45 @@ const Profile = () => {
                   cy='60'
                   r='59.5'
                   stroke='#035EAE'
-                  stroke-linejoin='round'
+                  strokeLinejoin='round'
                 />
               </svg>
               <div
                 className='second w-[100px] h-[100px] flex justify-center items-center m-[10px] rounded-[50px] bg-[#035EAE] 
-                drop-shadow-[1px 5px 10px rgba(56, 107, 152, 0.15)] fill-[#035EAE]'
+                drop-shadow-2xl cursor-pointer'
+                onClick={handleClick}
               >
-                <Image
-                  className='text-white'
-                  src='/camera.svg'
-                  width={24}
-                  height={24}
-                  alt='Picture of the user'
-                />
+                {previewImage ? (
+                  <Image
+                    src={previewImage}
+                    alt='Profile Preview'
+                    width={24}
+                    height={24}
+                    className='rounded-[50px]'
+                  />
+                ) : (
+                  <div className='flex justify-center items-center'>
+                    <Image
+                      src='/camera.svg'
+                      width={24}
+                      height={24}
+                      alt='Picture of the user'
+                    />
+                  </div>
+                )}
               </div>
             </div>
           </div>
         </div>
       </section>
+      <input
+        id='profileImageInput'
+        type='file'
+        className='hidden'
+        onChange={handleImageChange}
+      />
     </>
   );
 };
+
 export default Profile;
