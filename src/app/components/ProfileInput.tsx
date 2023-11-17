eb1cf7f76b4bb9e29ef6f46e9abed16e37258c0f
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-17 15:31:35 +0100

    Added profile input

---
+++ src/app/components/ProfileInput.tsx
@@ -0,0 +1,45 @@
+const ProfileInput = () => {
+  return (
+    <>
+      <section className='flex flex-col justify-center gap-1 mx-[41px] my-14'>
+        <form>
+          <div className='flex flex-col items-start gap-1'>
+            <label htmlFor='' className='label'>
+              Username
+            </label>
+            <input
+              type='text'
+              placeholder='Enter your username'
+              className='border-bottom flex items-center gap-2 self-stretch px-0 py-3 mb-6'
+            />
+            <label htmlFor='' className='label'>
+              First Name
+            </label>
+            <input
+              type='text'
+              placeholder='Enter your first name'
+              className='border-bottom flex items-center gap-2 self-stretch px-0 py-3 mb-6'
+            />
+            <label htmlFor='' className='label'>
+              Last Name
+            </label>
+            <input
+              type='text'
+              placeholder='Enter your last name'
+              className='border-bottom flex items-center gap-2 self-stretch px-0 py-3 mb-6'
+            />
+            <label htmlFor='' className='label'>
+              Date of Birth
+            </label>
+            <input
+              type='date'
+              placeholder='Enter your date of birth'
+              className='border-bottom flex items-center gap-2 self-stretch px-0 py-3 mb-6'
+            />
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+export default ProfileInput;
