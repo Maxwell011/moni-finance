f24fe27200b82bd00a56b1d3288bc34f1726cb3f
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-17 10:26:09 +0100

    Started profile image container

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,10 @@ const Profile = () => {
     <>
       <section className='bg-[#035EAE] w-full h-[246px] lg:w-full'>
         <div className='w-full h-[189px] rounded-[500px_500px_0px_0px] bg-white absolute top-[58px]'>
-          profile
+          {/* Profile image */}
+          <div className="">
+            
+          </div>
         </div>
       </section>
     </>
