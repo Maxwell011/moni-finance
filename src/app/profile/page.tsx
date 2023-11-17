d5f37a54912d1d7d67709c204fe73ae2c3cde9e5
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-17 10:19:06 +0100

    Created profile heading design

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+const Profile = () => {
+  return (
+    <>
+      <section className='bg-[#035EAE] w-full h-[246px] lg:w-full'>
+        <div className='w-full h-[189px] rounded-[500px_500px_0px_0px] bg-white absolute top-[58px]'>
+          profile
+        </div>
+      </section>
+    </>
+  );
+};
+export default Profile;
