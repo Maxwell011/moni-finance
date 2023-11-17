81a89609fd3f962a60af4ae5de75da64a1ce2f7c
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-17 11:42:28 +0100

    Added Profile image component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,15 @@
+import ImageBody from "../svg/ImageBody";
+
 const Profile = () => {
   return (
     <>
       <section className='bg-[#035EAE] w-full h-[246px] lg:w-full'>
         <div className='w-full h-[189px] rounded-[500px_500px_0px_0px] bg-white absolute top-[58px]'>
           {/* Profile image */}
-          <div className=''>
-            <div className='first flex items-center justify-center mt-12'>
+          <div className='flex items-center justify-center mt-12'>
+            <div className='first'>
               <svg
+                className='absolute'
                 xmlns='http://www.w3.org/2000/svg'
                 width='120'
                 height='120'
@@ -21,6 +24,9 @@ const Profile = () => {
                   stroke-linejoin='round'
                 />
               </svg>
+              <div className='second'>
+                <ImageBody />
+              </div>
             </div>
           </div>
         </div>
