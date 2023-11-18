b61c30754b8a8d37b9fa6728a2c59df6ebd0eaee
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-18 23:37:51 +0100

    Added cion svg

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,36 @@
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <section>
+        <div className=''>
+          <div className='images'>
+            <Image
+              src='/home.svg'
+              alt='Home Image'
+              width={24}
+              height={24}
+              priority
+            />
+            <Image
+              src='/send-2.svg'
+              alt='send image'
+              width={24}
+              height={24}
+              priority
+            />
+            <Image
+              src='/user.svg'
+              alt='user profile image'
+              width={24}
+              height={24}
+              priority
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 export default Footer;
