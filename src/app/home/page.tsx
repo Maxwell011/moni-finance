dcda46cd90ef94304d899316e31089cfeaee43a8
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-19 14:23:50 +0100

    Add Balance component to Home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,16 @@
+import Balance from "../components/Balance";
 import Features from "../components/Features";
 
 const Home = () => {
-  return <>
-  <section>
-    <div>
-      <Features/>
-    </div>
-  </section>
-  </>;
+  return (
+    <>
+      <section className='flex items-center justify-center'>
+        <div>
+          <Balance />
+          <Features />
+        </div>
+      </section>
+    </>
+  );
 };
 export default Home;
