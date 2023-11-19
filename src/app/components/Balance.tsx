dd3803370dc16539fc3c26f779d677b552b709cf
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-19 21:30:05 +0100

    Fix margin in Balance component

--- src/app/components/Balance.tsx
+++ src/app/components/Balance.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Balance = () => {
   return (
     <>
-      <section className='w-[312px] h-[136px] rounded-[15px] bg-gradient-to-r from-blue-900 to-blue-400 ml-[37px] mr-[25px] mt-[27px] mb-6'>
+      <section className='w-[312px] h-[136px] rounded-[15px] bg-gradient-to-r from-blue-900 to-blue-400 ml-[37px] mr-[25px] mt-[-15px] mb-6'>
         <div className='p-10'>
           <div className='text-white text-xs'>Available Balance</div>
           <div className='text-white text-[32px] flex gap-[30px]'>
