c7495313f8838478d691257e2e5d4074300cb498
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-19 12:57:35 +0100

    Finished feature component

--- src/app/components/Features.tsx
+++ src/app/components/Features.tsx
@@ -6,7 +6,7 @@ const Features = () => {
       <section>
         <div className='flex flex-col gap-1'>
           <div className='icons flex justify-around items-center'>
-            <div className='bg-[#E4F3FF] w-11 h-11 rounded-[30px] flex items-center justify-center'>
+            <div className='feature-blue'>
               <Image
                 src='/send-2a.svg'
                 alt='Home Image'
@@ -15,7 +15,7 @@ const Features = () => {
                 priority
               />
             </div>
-            <div className='bg-[#E4F3FF] w-11 h-11 rounded-[30px] flex items-center justify-center'>
+            <div className='feature-blue'>
               <Image
                 src='/add-square.svg'
                 alt='send image'
@@ -35,35 +35,41 @@ const Features = () => {
             </div>
           </div>
           <div className='flex justify-around items-center'>
-            <h3 className='text-[#0476D9] text-sm'>send</h3>
+            <h3 className='text-[#28374B] text-sm'>send</h3>
             <h3 className='text-[#28374B] text-sm'>Top-Up</h3>
             <h3 className='text-[#28374B] text-sm'>Bills</h3>
           </div>
           <div className='icons flex justify-around items-center'>
-            <Image
-              src='/chart-square.svg'
-              alt='Home Image'
-              width={16}
-              height={16}
-              priority
-            />
-            <Image
-              src='/sort.svg'
-              alt='send image'
-              width={16}
-              height={16}
-              priority
-            />
-            <Image
-              src='/money-2.svg'
-              alt='user profile image'
-              width={16}
-              height={16}
-              priority
-            />
+            <div className='feature-yellow'>
+              <Image
+                src='/chart-square.svg'
+                alt='Home Image'
+                width={16}
+                height={16}
+                priority
+              />
+            </div>
+            <div className='feature-yellow'>
+              <Image
+                src='/sort.svg'
+                alt='send image'
+                width={16}
+                height={16}
+                priority
+              />
+            </div>
+            <div className='feature-yellow'>
+              <Image
+                src='/money-2.svg'
+                alt='user profile image'
+                width={16}
+                height={16}
+                priority
+              />
+            </div>
           </div>
           <div className='flex justify-around items-center'>
-            <h3 className='text-[#0476D9] text-sm'>Data</h3>
+            <h3 className='text-[#28374B] text-sm'>Data</h3>
             <h3 className='text-[#28374B] text-sm'>Airtime</h3>
             <h3 className='text-[#28374B] text-sm'>Betting</h3>
           </div>
