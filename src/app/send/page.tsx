8e4920b31c9b8425467069062af3339731b7b31e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-20 13:22:41 +0100

    Added send features options

--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -7,28 +7,28 @@ const Send = () => {
       <section>
         <div>
           <div>
-            <h2></h2>
-            <p></p>
+            <h2>Bank Transfer</h2>
+            <p>Transfer to other banks</p>
           </div>
           <div>
-            <h2></h2>
-            <p></p>
+            <h2>Credit/Debit Card</h2>
+            <p>Transfer using your credit/debit cards</p>
           </div>
           <div>
-            <h2></h2>
-            <p></p>
+            <h2>Digital Wallets</h2>
+            <p>Paypal,Venmo, Cash App etc.</p>
           </div>
           <div>
-            <h2></h2>
-            <p></p>
+            <h2>Cryptocurrency</h2>
+            <p>Make transfer and manage your cryptocurrency</p>
           </div>
           <div>
-            <h2></h2>
-            <p></p>
+            <h2>Payment Gateway</h2>
+            <p>Transfer using Stripe, square etc.</p>
           </div>
           <div>
-            <h2></h2>
-            <p></p>
+            <h2>others</h2>
+            <p>Explore more payment options</p>
           </div>
         </div>
         <Transactions />
