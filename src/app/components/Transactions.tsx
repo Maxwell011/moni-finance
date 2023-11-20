e3edfb0df40a0f09bb6d407eb54909f17e17d128
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-20 15:55:01 +0100

    Update Transactions component styling

--- src/app/components/Transactions.tsx
+++ src/app/components/Transactions.tsx
@@ -11,10 +11,10 @@ const Transactions = () => {
               <Link href='/transactions'>View all</Link>
             </div>
           </div>
-          <div className='flex flex-col gap-[55px]'>
+          <div className='flex flex-col gap-[40px]'>
             <div className='flex gap-[5px] mx-[27px] -my-3'>
-              <div className='w-[35px] h-[35px] rounded-2xl bg-orange-600 flex items-center justify-center'>
-                <p>D</p>
+              <div className='w-[35px] h-[35px] rounded-2xl bg-[#EEEEEE4D] flex items-center justify-center'>
+                <p className='text-black'>D</p>
               </div>
               <div className='flex gap-28 items-center'>
                 <p className='text-sm text-[#28374b]'>Dave Martins</p>
