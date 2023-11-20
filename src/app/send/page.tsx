bcd05b59f9fd0334bd687bc4d6b5e5d88d2ed2c3
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-20 15:56:11 +0100

    Add navigation and update payment options layout

--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
+import Nav from "../components/Nav";
 import Transactions from "../components/Transactions";
 
 const Send = () => {
   return (
     <>
       <section>
+        <Nav/>
         <div>
-          <div className=''>
+          <div className='flex gap-5 items-center justify-center mb-5'>
             <div className='w-[146px] h-20 rounded-[7px] flex flex-col items-center justify-center gap-1 bg-[#E4F3FF] shadow-[1px_4px_15px_0px_rgba(3,94,174,0.10)]'>
               <h2 className='text-sm text-[#035EAE]'>Bank Transfer</h2>
               <p className='text-[#28374b] text-[10px]'>
@@ -20,7 +22,7 @@ const Send = () => {
               </p>
             </div>
           </div>
-          <div>
+          <div className='flex gap-5 items-center justify-center mb-5'>
             <div className='bg-[#F3EAFF] w-[146px] h-20 rounded-[7px] flex flex-col items-center justify-center gap-1 shadow-[1px_4px_10px_0px_rgba(115,23,236,0.10)]'>
               <h2 className='text-sm text-[#7317EC]'>Digital Wallets</h2>
               <p className='text-[#28374b] text-[10px] text-center'>
@@ -34,19 +36,19 @@ const Send = () => {
               </p>
             </div>
           </div>
-          <div>
+          <div className='flex gap-5 items-center justify-center mb-5'>
             <div className='bg-[#FFDBFE] w-[146px] h-20 rounded-[7px] flex flex-col items-center justify-center gap-1 shadow-[1px_4px_15px_0px_rgba(255,60,247,0.10)]'>
               <h2 className='text-sm text-[#FF3CF7] text-center'>
                 Payment Gateway
               </h2>
-              <p className='text-[#28374b] text-[10px]'>
-                Transfer using Stripe, square etc.
+              <p className='text-[#28374b] text-[10px] text-center'>
+                Transfer using Stripe, <br /> square etc.
               </p>
             </div>
             <div className='bg-[#F1F1F1] w-[146px] h-20 rounded-[7px] flex flex-col items-center justify-center gap-1 shadow-[1px_4px_15px_0px_rgba(0,0,0,0.10)]'>
               <h2 className='text-[#414c56] text-sm'>others</h2>
               <p className='text-[#28374b] text-[10px] text-center'>
-                Explore more payment options
+                Explore more <br /> payment options
               </p>
             </div>
           </div>
