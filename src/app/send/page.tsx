213752e1e2a908fea10383b4d9a39735f62fc360
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-20 13:14:49 +0100

    Add Send page component

---
+++ src/app/send/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer";
+import Transactions from "../components/Transactions";
+
+const Send = () => {
+  return (
+    <>
+    <section>
+      <div>
+
+      </div>
+      <Transactions/>
+      <Footer />
+    </section>
+    </>
+  );
+};
+export default Send;
