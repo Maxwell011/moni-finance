a57abfb987b2533f1e7f8d6b1901e7835e9b1db9
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-20 15:55:58 +0100

    Update Nav component styling and remove unnecessary code

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,11 +5,9 @@ const Nav = () => {
   return (
     <>
       <section>
-        <div className='flex items-center justify-between mx-6 my-[25px]'>
+        <div className='flex items-center justify-between mx-6 mt-[7px] mb-2.5'>
           <HamburgerButton />
-          <h2 className='text-[#28374B] text-base font-semibold'>
-            👋 Hello John
-          </h2>
+          <h2 className='text-[#28374B] text-base font-semibold'>Hello John</h2>
           <Image
             className='shadow-[1px_5px_10px_0px_rgba(56,107,152,0.15)] rounded-[40px]'
             src='/Ellipse 5.svg'
