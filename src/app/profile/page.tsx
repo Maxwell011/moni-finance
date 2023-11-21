8192325b912e38f9bbb3b7bd5cdd5e5f0144214f
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-21 12:58:37 +0100

    Update profile page settings icons

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,30 +10,38 @@ const Profile = () => {
           <h1 className='text-[#28374b] text-base'>Profile Settings</h1>
         </div>
         <div>
-          <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10'>
+          <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10 cursor-pointer'>
             <h3 className='text-sm text-[#28374b]'>Account Settings</h3>
             <Image
               src='/setting-2.svg'
               alt='Settings Icon'
               width={14}
               height={14}
+              className='hover:animate-spin'
             />
           </div>
-          <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10'>
+          <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10 cursor-pointer'>
             <h3 className='text-sm text-[#28374b]'>Security Settings</h3>
-            <Image src='/key.svg' alt='key image' width={14} height={14} />
+            <Image
+              src='/key.svg'
+              alt='key image'
+              width={14}
+              height={14}
+              className='hover:animate-bounce'
+            />
           </div>
           <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10'>
             <h3 className='text-sm text-[#28374b]'>Customization Settings</h3>
             <Image src='/edit.svg' alt='Edit image' width={14} height={14} />
           </div>
-          <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10'>
+          <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10 cursor-pointer'>
             <h3 className='text-sm text-[#28374b]'>Help and Support Center</h3>
             <Image
               src='/message-question.svg'
               alt='message question'
               width={14}
               height={14}
+              className='hover:animate-ping'
             />
           </div>
           <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#F82828] text-[#F82828] h-10'>
