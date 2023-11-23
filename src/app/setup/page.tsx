eaf16e16490b5c99755c71de1701d50d8ffe44c3
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-23 20:48:54 +0100

    Add setup page component

---
+++ src/app/setup/page.tsx
@@ -0,0 +1,20 @@
+const page = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <h1 className='text-2xl text-[#28374b]'>
+            You are almost ready to start transacting
+          </h1>
+          <p className='text-sm'>
+            To start transacting, you will need to complete your account setup.
+            You can choose to skip this process and go straight to your
+            dashboard but you will not be able to transact until you complete
+            the process.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+export default page;
