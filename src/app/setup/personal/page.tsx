5d3d70b24904e5d6d9c0e1a42f5a9b050b8b5815
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-24 15:53:40 +0100

    Refactor personal information form layout

--- src/app/setup/personal/page.tsx
+++ src/app/setup/personal/page.tsx
@@ -4,12 +4,18 @@ import Link from "next/link";
 export default function PersonalInformation() {
   return (
     <>
-      <section>
-        <Image src='/arrow-left.svg' alt='Arrow left' width={24} height={24} />
+      <section className='m-[41px]'>
+        <Image
+          src='/arrow-left.svg'
+          alt='Arrow left'
+          width={24}
+          height={24}
+          className='mb-6'
+        />
         <div>
-          <p className='text-sm text-[#5e6978]'>Step 1 of 4</p>
-          <h1 className='text-2xl text-[#28374B]'>Personal Information</h1>
-          <p className='text-sm text-[#939BA5]'>
+          <p className='text-sm text-[#5e6978] mb-4'>Step 1 of 4</p>
+          <h1 className='text-2xl text-[#28374B]mb-2'>Personal Information</h1>
+          <p className='text-sm text-[#939BA5] mb-[30px]'>
             Welcome to the KYC process. We want to ensure the safety and
             security of all our users
           </p>
@@ -19,9 +25,8 @@ export default function PersonalInformation() {
           <input
             type='text'
             placeholder='Enter Your Full Name'
-            className='w-full sm:w-36 h-10 sm:h-[41px] input-style flex items-center border rounded-[7px] border-solid border-[#28374B]'
+            className='w-full sm:w-36 h-10 sm:h-[41px] input-style flex items-center border rounded-[7px] border-solid border-[#28374B] mb-[25px]'
           />
-          <p>Your name as it appears on your Id</p>
           <label className='text-[#28374B] lg:text-xl  sm:text-sm not-italic font-normal leading-[normal]'>
             Date of Birth
           </label>
@@ -29,19 +34,17 @@ export default function PersonalInformation() {
             type='date'
             name='birth'
             placeholder='Enter your date of birth'
-            className='w-full sm:w-36 h-10 sm:h-[41px] input-style flex items-center border rounded-[7px] border-solid border-[#28374B]'
+            className='w-full sm:w-36 h-10 sm:h-[41px] input-style flex items-center border rounded-[7px] border-solid border-[#28374B] mb-[25px]'
           />
-          <p>Your date of birth in this format DD/MM/YYYY</p>
           <label className='text-[#28374B] sm:text-xl lg:tex-2xl not-italic font-normal leading-[normal]'>
             Phone Number
           </label>
           <input
             type='text'
             placeholder='Enter your phone number'
-            className='w-full sm:w-36 h-10 sm:h-[41px] input-style flex items-center border rounded-[7px] border-solid border-[#28374B]'
+            className='w-full sm:w-36 h-10 sm:h-[41px] input-style flex items-center border rounded-[7px] border-solid border-[#28374B] mb-[40px]'
           />
-          <p>Please provide a valid phone number</p>
-          <button className='flex w-[278px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] bg-[#0476D9]'>
+          <button className='flex w-[294px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] bg-[#0476D9]'>
             <Link href='/setup/address' legacyBehavior>
               Next
             </Link>
