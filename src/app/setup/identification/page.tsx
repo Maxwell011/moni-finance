5c3f8b5ea94a0b0ee762a538b06c2b9057d11ba9
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-25 20:03:31 +0100

    added  identification page styling

--- src/app/setup/identification/page.tsx
+++ src/app/setup/identification/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function Identification() {
-  return <div>page</div>;
+  const router = useRouter();
+
+  return (
+    <>
+      <section className='mx-[41px] my-[30px]'>
+        <div>
+          <div
+            className='cursor-pointer'
+            onClick={() => router.push("/setup/personal")}
+          >
+            <Image
+              src='/arrow-left.svg'
+              alt='Arrow left'
+              width={24}
+              height={24}
+              className='mb-6'
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
