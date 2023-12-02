edd7a5bb4a2423ca22ec88703fd0932254010e16
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-02 22:40:50 +0100

    Refactor HamburgerButton component

--- src/app/svg/Hamburger.tsx
+++ src/app/svg/Hamburger.tsx
@@ -22,43 +22,45 @@ function HamburgerButton() {
         height={24}
         priority
       />
-      { <button
-        className='flex items-center justify-center w-14 h-14 p-0 rounded-full bg-transparent border-0 transition duration-200 ease-in-out cursor-pointer hover:bg-gray-800 focus:outline-none focus-visible:bg-gray-800'
-        onClick={handleHamburgerClick}
-      >
-        <svg
-          className='w-3/4'
-          aria-hidden='true'
-          viewBox='0 0 24 24'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
+      {
+        <button
+          className='flex items-center justify-center w-14 h-14 p-0 rounded-full bg-transparent border-0 transition duration-200 ease-in-out cursor-pointer hover:bg-gray-800 focus:outline-none focus-visible:bg-gray-800'
+          onClick={handleHamburgerClick}
         >
-          <rect
-            width='18'
-            height='1.5'
-            className='bg-red-500'
-            rx='0.75'
-            x='3'
-            y='6.25'
-          />
-          <rect
-            width='18'
-            height='1.5'
-            className='bg-red-500'
-            rx='0.75'
-            x='3'
-            y='11.25'
-          />
-          <rect
-            width='18'
-            height='1.5'
-            className='bg-red-500'
-            rx='0.75'
-            x='3'
-            y='16.25'
-          />
-        </svg>
-      </button>
+          <svg
+            className='w-3/4'
+            aria-hidden='true'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+          >
+            <rect
+              width='18'
+              height='1.5'
+              className='bg-red-500'
+              rx='0.75'
+              x='3'
+              y='6.25'
+            />
+            <rect
+              width='18'
+              height='1.5'
+              className='bg-red-500'
+              rx='0.75'
+              x='3'
+              y='11.25'
+            />
+            <rect
+              width='18'
+              height='1.5'
+              className='bg-red-500'
+              rx='0.75'
+              x='3'
+              y='16.25'
+            />
+          </svg>
+        </button>
+      }
 
       {isSideNavOpen && (
         <div className='side-nav fixed top-0 left-0 w-56 h-full bg-gray-200 transform transition-transform duration-300 ease-in-out translate-x-0'>
@@ -74,7 +76,7 @@ function HamburgerButton() {
             <li className='p-4'>Menu Item 3</li>
           </ul>
         </div>
-      )}}
+      )}
     </div>
   );
 }
