ef829f44346f8e826fc9468421d45365f0179156
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-04 16:09:02 +0100

    Changed loading page style

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ export default function Loader() {
     <section>
       <Suspense
         fallback={
-          <p className='bg-slate-700 text-6xl text-red-700'>
+          <p className='bg-black text-6xl text-red-700'>
             <Image
               src='/Colorful-Logo 1 (1).svg'
               alt='Document image'
@@ -14,7 +14,7 @@ export default function Loader() {
               height={83}
               className=''
             />
-            Loading the launch screen...
+            Loading the launch screen
           </p>
         }
       ></Suspense>
