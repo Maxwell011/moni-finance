be5ab0360dc6fbcaef54d7f2b510384167cc5a7d
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-05 15:38:23 +0100

    Added loaders to setup

---
+++ src/app/setup/loading.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Suspense } from "react";
+
+export default function Loader() {
+  return (
+    <section>
+      <Suspense
+        fallback={
+          <p className='bg-black text-6xl text-red-700'>
+            Loading the launch screen
+          </p>
+        }
+      >
+        <div className='absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4'>
+          <Image
+            src='/Colorful-Logo 1 (1).svg'
+            alt='Document image'
+            width={96}
+            height={83}
+            className=''
+          />
+        </div>
+      </Suspense>
+    </section>
+  );
+}
