d8a46c6d6172bae37c23b124c6b03c0ed6dd30c9
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-07 22:25:47 +0100

    update element

--- src/app/transaction/page.tsx
+++ src/app/transaction/page.tsx
@@ -44,6 +44,15 @@ const Transaction = () => {
             <p className='text-[#03a63c] text-sm'>+N9,400.00</p>
           </div>
         </div>
+        <div className='flex gap-[5px] mx-[27px] -my-3'>
+          <div className='w-[35px] h-[35px] rounded-2xl bg-[#EEEEEE4D] flex items-center justify-center'>
+            <p className='text-black'>C</p>
+          </div>
+          <div className='flex gap-28 items-center'>
+            <p className='text-sm text-[#28374b]'>Clems Miley</p>
+            <p className='text-[#03a63c] text-sm'>+N9,400.00</p>
+          </div>
+        </div>
       </div>{" "}
     </div>
   );
