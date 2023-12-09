2a9481389ee69dba9723bfbd2910320dedccbf13
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-09 22:53:02 +0100

    Updated desktop styling

--- src/app/setup/personal/page.tsx
+++ src/app/setup/personal/page.tsx
@@ -56,7 +56,7 @@ export default function PersonalInformation() {
             />
           </div>
 
-          <button className='flex w-[294px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] bg-[#0476D9]'>
+          <button className='flex w-[294px] justify-center items-center gap-4 text-white text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] bg-[#0476D9] lg:w-[30%]'>
             <Link href='/setup/address' legacyBehavior>
               Next
             </Link>
