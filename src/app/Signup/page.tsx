ffcfdeb197ed4825527d14a249700c50acb98767
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-11 22:20:53 +0100

    updated sign up page

--- src/app/Signup/page.tsx
+++ src/app/Signup/page.tsx
@@ -11,7 +11,7 @@ const Signup = () => {
         <input
           type='text'
           placeholder='Enter Your Email'
-          className='w-full sm:w-36 h-10 sm:h-[41px] input-style mb-4 lg:w-full'
+          className='w-full sm:w-36 h-10 sm:h-[41px] input-style mb-4 lg:w-[130%]'
         />
         <label className='sm:text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
           Password
@@ -19,13 +19,13 @@ const Signup = () => {
         <input
           type='password'
           placeholder='Enter Your Password'
-          className='w-full sm:w-36 h-10 sm:h-[41px] input-style lg:w-full'
+          className='w-full sm:w-36 h-10 sm:h-[41px] input-style lg:w-[130%]'
         />
         <h3 className='text-[#939BA5] text-xs not-italic font-normal leading-[normal] w-[300px] mt-1'>
           Password must be a minimum of eight characters contain at least one
           uppercase letter, one lowercase letter and one number
         </h3>
-        <button className='inactive-btn flex justify-center items-center gap-4 text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] sm:w-36 w-full mt-12 lg:w-full'>
+        <button className='inactive-btn flex justify-center items-center gap-4 text-base not-italic font-normal leading-[normal] p-3 rounded-[10px] sm:w-36 w-full mt-12 lg:w-[130%]'>
           Sign Up
         </button>
       </div>
