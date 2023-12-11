29bf5fbdbad227c2892e6545e5c8cea9dff94a9a
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-11 23:28:02 +0100

    updated font style

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,12 +9,16 @@ const Profile = () => {
       <section>
         <div className='flex items-center gap-20 m-8'>
           <HamburgerButton />
-          <h1 className='text-[#28374b] text-base'>Account Settings</h1>
+          <h1 className='text-[#28374b] text-base font-semibold'>
+            Account Settings
+          </h1>
         </div>
         <div>
           <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10 cursor-pointer'>
             <Link href='/profile/profilesetting'>
-              <h3 className='text-sm text-[#28374b]'>Profile Settings</h3>
+              <h3 className='text-sm text-[#28374b] font-semibold'>
+                Profile Settings
+              </h3>
             </Link>
             <Image
               src='/setting-2.svg'
@@ -25,7 +29,9 @@ const Profile = () => {
             />
           </div>
           <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10 cursor-pointer'>
-            <h3 className='text-sm text-[#28374b]'>Security Settings</h3>
+            <h3 className='text-sm text-[#28374b] font-semibold'>
+              Security Settings
+            </h3>
             <Image
               src='/key.svg'
               alt='key image'
@@ -35,11 +41,15 @@ const Profile = () => {
             />
           </div>
           <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10'>
-            <h3 className='text-sm text-[#28374b]'>Customization Settings</h3>
+            <h3 className='text-sm text-[#28374b] font-semibold'>
+              Customization Settings
+            </h3>
             <Image src='/edit.svg' alt='Edit image' width={14} height={14} />
           </div>
           <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#28374B] h-10 cursor-pointer'>
-            <h3 className='text-sm text-[#28374b]'>Help and Support Center</h3>
+            <h3 className='text-sm text-[#28374b] font-semibold'>
+              Help and Support Center
+            </h3>
             <Image
               src='/message-question.svg'
               alt='message question'
@@ -49,7 +59,7 @@ const Profile = () => {
             />
           </div>
           <div className='flex w-[312px] justify-between items-center border m-[31px] px-6 py-3 rounded-[7px] border-solid border-[#F82828] text-[#F82828] h-10'>
-            <h3 className='text-[#F82828]'>Deactivate Account</h3>
+            <h3 className='text-[#F82828] font-semibold'>Deactivate Account</h3>
             <Image
               src='/lock-slash.svg'
               alt='lock icon'
