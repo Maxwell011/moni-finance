ca34e201aa6c483bfd4c50aa0ee373ab146dade6
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-11 22:33:39 +0100

    updated sign up page

--- src/app/Signup/page.tsx
+++ src/app/Signup/page.tsx
@@ -2,7 +2,7 @@ const Signup = () => {
   return (
     <>
       <div className='flex flex-col justify-center gap-1 mx-[41px] my-0 lg:absolute lg:-translate-x-2/4 lg:-translate-y-2/4 lg:left-2/4 lg:top-2/4'>
-        <h2 className='text-[#28374B] text-[32px] not-italic font-semibold leading-[normal] mb-10'>
+        <h2 className='text-[#28374B] text-[32px] not-italic font-semibold leading-[normal] mb-10 lg:text-[30px]'>
           Sign Up
         </h2>
         <label className='text-[#28374B] text-xl not-italic font-normal leading-[normal]'>
@@ -21,7 +21,7 @@ const Signup = () => {
           placeholder='Enter Your Password'
           className='w-full sm:w-36 h-10 sm:h-[41px] input-style lg:w-[130%]'
         />
-        <h3 className='text-[#939BA5] text-xs not-italic font-normal leading-[normal] w-[300px] mt-1'>
+        <h3 className='text-[#939BA5] text-xs not-italic font-normal leading-[normal] w-[300px] mt-1 lg:text-[17px]'>
           Password must be a minimum of eight characters contain at least one
           uppercase letter, one lowercase letter and one number
         </h3>
