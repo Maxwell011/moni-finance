9660f3fb8b58d8d10e86f9d5b398ba309e71005d
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-28 23:45:48 +0100

    Update loading message in setup component

--- src/app/setup/loading.tsx
+++ src/app/setup/loading.tsx
@@ -7,7 +7,7 @@ export default function Loader() {
       <Suspense
         fallback={
           <p className='bg-black text-6xl text-red-700'>
-            loading...
+            loading content...
           </p>
         }
       >
