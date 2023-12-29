39a6f6a993590dd46a6009407c7fb5ddef7b0376
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-29 22:32:27 +0100

    Remove commented out code in Loader component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -11,7 +11,7 @@ export default function Loader() {
           </p>
         }
       >
-        <div className="absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4">
+        {/* <div className="absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4">
           <Image
             src="/Colorful-Logo 1 (1).svg"
             alt="Document image"
@@ -20,7 +20,7 @@ export default function Loader() {
             priority={true}
             className=""
           />
-        </div>
+        </div> */}
       </Suspense>
     </section>
   );
