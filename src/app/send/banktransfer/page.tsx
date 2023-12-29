09021445f31663277b9b2300129005cb038af9b3
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-29 23:41:30 +0100

    Add input for bank transfer and update comments

--- src/app/send/banktransfer/page.tsx
+++ src/app/send/banktransfer/page.tsx
@@ -2,7 +2,7 @@ export default function BankTransfer() {
   return (
     <>
       <h1>Bank Transfer</h1>
-      {/* I will add an input and check out globus or way other banks send money */}
+      {/* I will add an input and check out globus or way other banks send money that is from bank to bank */}
     </>
   );
 }
